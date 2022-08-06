492401d99956129378a9776a51c7b15c13b5393b
kashyaprajan11 <rkashyap251@gmail.com>
2022-08-07 00:11:02 +0530

    Put the key

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,4 @@
-const API_KEY = // Your API Key goes here
-
+const API_KEY = f66d187fcae64fb438e1e89e0b7c5a4a;
 
 export default {
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
